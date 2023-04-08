a6be812a08be9792fc0a7e1803fcea143e3e5a93
Coral Kuta <coral.kuta@live.biu.ac.il>
2023-04-08 18:15:00 +0300

    done with 1-4. starting of 5

--- mlp1.py
+++ mlp1.py
@@ -3,9 +3,38 @@ import numpy as np
 STUDENT={'name': 'YOUR NAME',
          'ID': 'YOUR ID NUMBER'}
 
+
+def softmax(x):
+    """
+    Compute the softmax vector.
+    x: a n-dim vector (numpy array)
+    returns: an n-dim vector (numpy array) of softmax values
+    """
+    # Your code should be fast, so use a vectorized implementation using numpy,
+    # don't use any loops.
+    # With a vectorized implementation, the code should be no more than 2 lines.
+    #
+    # For numeric stability, use the identify you proved in Ex 2 Q1.
+
+    # because softmax(x) = softmax(x-c) for any constant c,
+    # we can subtract the maximum value of x from each element of x.
+    # this does not change the result of softmax, but it makes the numbers in x much smaller,
+    # which is more numerically stable.
+
+    # We will use the identity: softmax(x) = softmax(x+c).
+    # We will use c = the minus of the largest logit.
+    new_x = x - np.max(x)
+    np_exp = np.exp(new_x)
+    return np_exp / np.sum(np_exp)
+
+
+def tanh_prime(x):
+    return 1 - np.tanh(x) ** 2
+
+
 def classifier_output(x, params):
-    # TODO: YOU CODE HERE
-    return probs
+    W1, b1, W2, b2 = params
+    return softmax(np.dot(W2.T, np.tanh(np.dot(W1.T, x) + b1)) + b2)
 
 def predict(x, params):
     """
